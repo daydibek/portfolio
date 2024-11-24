70738b6f809c4109deca70e4ca79d9717740a7fe
daydibek <daydibek1228@gmail.com>
2024-11-24 07:57:06 +0100

    "a few changes committed"

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const navLogo = document.getElementById("navLogo");
@@ -8,15 +7,4 @@ const author = document.getElementById("author");
 navLogo.addEventListener("click", function(){
 navList.classList.toggle("hidden");
 author.classList.toggle("hidden");
-=======
-"use strict";
-
-const navLogo = document.getElementById("navLogo");
-const navList = document.getElementById("navList");
-const author = document.getElementById("author");
-
-navLogo.addEventListener("click", function(){
-navList.classList.toggle("hidden");
-author.classList.toggle("hidden");
->>>>>>> 0e39eeb2bc578df8bc2bb213651dfd097f2de46c
-});
+})
