ccdb6171f0deb633f09fcb5809240e6b075cad95
daydibek <110773190+daydibek@users.noreply.github.com>
2024-11-24 07:07:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+"use strict";
+
+const navLogo = document.getElementById("navLogo");
+const navList = document.getElementById("navList");
+const author = document.getElementById("author");
+
+navLogo.addEventListener("click", function(){
+navList.classList.toggle("hidden");
+author.classList.toggle("hidden");
+=======
+"use strict";
+
+const navLogo = document.getElementById("navLogo");
+const navList = document.getElementById("navList");
+const author = document.getElementById("author");
+
+navLogo.addEventListener("click", function(){
+navList.classList.toggle("hidden");
+author.classList.toggle("hidden");
+>>>>>>> 0e39eeb2bc578df8bc2bb213651dfd097f2de46c
+});
